c21086cc6f4ac94dd0e738b1101653d4afcac296
Jorrit05 <jorstut@hotmail.com>
2022-02-02 19:07:13 +0100

    day 3

--- advent-of-code/day3/energy.hs
+++ advent-of-code/day3/energy.hs
@@ -1,22 +1,28 @@
 import System.IO
-import Control.Monad
+import Data.List
+import Data.Char
 
+scanString :: String -> [Int]
+scanString = map digitToInt
 
+countDigits :: [Int] -> (Int, Int)
+countDigits xs | ones > zeros = (1, 0)
+               | otherwise  = (0, 1)
+               where
+                   ones = length $ filter (==1) xs
+                   zeros = length $ filter (==0) xs
 
-readInt :: String -> Int
-readInt = read
+bit2int :: [Int] -> Int
+bit2int = foldr (\x y -> x + 2 * y) 0
 
--- myHead :: [String] -> Int -> Char
--- myHead xs = return xs !! 2
--- transpose matrix
--- fold over elke rij.
+getBinaries :: [(Int,Int)] -> (Int,Int)
+getBinaries xs = (bit2int . reverse $ map fst xs, bit2int . reverse $ map snd xs)
 
 main = do
     -- fileContent is one large string.
-    fileContent <- readFile "test.txt"
+    fileContent <- readFile "input.txt"
     -- Break up the string to a list using the 'words' function.
-    -- convert to 'Int' and insert to a list
-    let bitList =  words fileContent
-    let a = map head bitList
-    -- b <- foldr (!!(2)) bitList
-    return a
+    -- convert to 'Int', insert to a multidimensional list and transpose.
+    let bitList =  transpose $ map scanString $ words fileContent
+    let puzzle1 = getBinaries $ map countDigits bitList
+    return (uncurry (*) puzzle1)
